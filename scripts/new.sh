faf7c77263ba15e30639163b4754bac8e4144df9
juliannemarik <julianne.marik@gmail.com>
2021-09-05 19:05:45 -0500

    move new dictionary fixtures

--- scripts/new.sh
+++ scripts/new.sh
@@ -3,17 +3,17 @@ echo "Please enter a name for the new dictionary package:"
 read name
 
 PACKAGE_DIRECTORY_PATH="${PWD}/packages/${name}"
-TEMPLATE_DIRECTORY_PATH="${PWD}/templates/newDictionary"
+TEMPLATE_DIRECTORY_PATH="${PWD}/scripts/fixtures"
 
 # build package structure
 mkdir "${PACKAGE_DIRECTORY_PATH}"
 mkdir "${PACKAGE_DIRECTORY_PATH}/src"
-cp "${TEMPLATE_DIRECTORY_PATH}/indexTemplate.ts" "${PACKAGE_DIRECTORY_PATH}/src/index.ts"
-cp "${TEMPLATE_DIRECTORY_PATH}/dictionaryTemplate.ts" "${PACKAGE_DIRECTORY_PATH}/src/dictionary.ts"
-cp "${TEMPLATE_DIRECTORY_PATH}/typesTemplate.ts" "${PACKAGE_DIRECTORY_PATH}/src/types.ts"
-cp "${TEMPLATE_DIRECTORY_PATH}/rollupTemplate.json" "${PACKAGE_DIRECTORY_PATH}/src/rollup.json"
-cp "${TEMPLATE_DIRECTORY_PATH}/tsconfigTemplate.json" "${PACKAGE_DIRECTORY_PATH}/tsconfig.json"
-cp "${TEMPLATE_DIRECTORY_PATH}/packageJsonTemplate.json" "${PACKAGE_DIRECTORY_PATH}/package.json"
+cp "${TEMPLATE_DIRECTORY_PATH}/index.ts" "${PACKAGE_DIRECTORY_PATH}/src/index.ts"
+cp "${TEMPLATE_DIRECTORY_PATH}/dictionary.ts" "${PACKAGE_DIRECTORY_PATH}/src/dictionary.ts"
+cp "${TEMPLATE_DIRECTORY_PATH}/types.ts" "${PACKAGE_DIRECTORY_PATH}/src/types.ts"
+cp "${TEMPLATE_DIRECTORY_PATH}/rollup.json" "${PACKAGE_DIRECTORY_PATH}/src/rollup.json"
+cp "${TEMPLATE_DIRECTORY_PATH}/tsconfig.json" "${PACKAGE_DIRECTORY_PATH}/tsconfig.json"
+cp "${TEMPLATE_DIRECTORY_PATH}/package.json" "${PACKAGE_DIRECTORY_PATH}/package.json"
 
 # update files to be package-specific
 ts-node "${PWD}/scripts/replace.ts" "${PACKAGE_DIRECTORY_PATH}/package.json" "PACKAGE_NAME" "${name}"
