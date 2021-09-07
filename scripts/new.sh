d8cf152a952be19a96f1e027bbe374c6b14e6beb
juliannemarik <julianne.marik@gmail.com>
2021-09-07 10:56:04 -0500

    test making change to root package

--- scripts/new.sh
+++ scripts/new.sh
@@ -1,11 +1,11 @@
-# Prompt user for name of package
+# Prompt user for name of package:
 echo "Please enter a name for the new dictionary package:"
 read name
 
 PACKAGE_DIRECTORY_PATH="${PWD}/packages/${name}"
 TEMPLATE_DIRECTORY_PATH="${PWD}/scripts/fixtures"
 
-# build package structure
+# build package structure:
 mkdir "${PACKAGE_DIRECTORY_PATH}"
 mkdir "${PACKAGE_DIRECTORY_PATH}/src"
 cp "${TEMPLATE_DIRECTORY_PATH}/index.ts" "${PACKAGE_DIRECTORY_PATH}/src/index.ts"
@@ -15,7 +15,7 @@ cp "${TEMPLATE_DIRECTORY_PATH}/rollup.json" "${PACKAGE_DIRECTORY_PATH}/src/rollu
 cp "${TEMPLATE_DIRECTORY_PATH}/ts_config.json" "${PACKAGE_DIRECTORY_PATH}/tsconfig.json"
 cp "${TEMPLATE_DIRECTORY_PATH}/package.json" "${PACKAGE_DIRECTORY_PATH}/package.json"
 
-# update files to be package-specific
+# update files to be package-specific:
 ts-node "${PWD}/scripts/replace.ts" "${PACKAGE_DIRECTORY_PATH}/package.json" "PACKAGE_NAME" "${name}"
 ts-node "${PWD}/scripts/replace.ts" "${PACKAGE_DIRECTORY_PATH}/src/index.ts" "\/\/\s" ""
 ts-node "${PWD}/scripts/replace.ts" "${PACKAGE_DIRECTORY_PATH}/src/dictionary.ts" "\/\/\s" ""
