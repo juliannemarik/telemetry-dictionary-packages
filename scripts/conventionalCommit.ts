84b01a654fd2d38b84b99a3a490b38a405bd396b
juliannemarik <julianne.marik@gmail.com>
2021-09-08 10:10:42 -0500

    fix(root): uncomment publish action

--- scripts/conventionalCommit.ts
+++ scripts/conventionalCommit.ts
@@ -76,7 +76,6 @@ ${breaking ? `BREAKING CHANGE: ${breakingDescription} \n` : ''}
 }
 
 const init = async () => {  
-  console.log('INIT!!!');
   const answers = await prompt(questions);
   const commitMessage = generateCommitMessage(answers);
   
