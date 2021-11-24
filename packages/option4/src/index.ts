e7c2046daee699f020468db0ad8af0f5ad892667
juliannemarik <julianne.marik@gmail.com>
2021-11-24 13:16:28 -0600

    feat(option4): add a new dictionary

---
+++ packages/option4/src/index.ts
@@ -0,0 +1,6 @@
+import telemetryDictionary from './dictionary.json';
+import telemetryConstants from './constants.json';
+import { read, write } from './dictionary';
+
+export const dictionary = telemetryDictionary;
+export const CONSTANTS = telemetryConstants;
