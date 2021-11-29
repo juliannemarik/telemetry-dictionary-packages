c0f0f611e5a332016994195c2f4f714e0e90c531
juliannemarik <julianne.marik@gmail.com>
2021-11-29 08:47:07 -0600

    feat(base): add a new dictionary

--- packages/new6/src/index.ts
+++
@@ -1,7 +0,0 @@
-import telemetryDictionary from './dictionary.json';
-import telemetryConstants from './constants.json';
-import { testingDictionary } from './dictionary';
-
-export const dictionary = telemetryDictionary;
-export const testDictionary = testingDictionary;
-export const CONSTANTS = telemetryConstants;
