a7e4e4f410c36c0ee8db9327d9a0530417e36a7f
juliannemarik <julianne.marik@gmail.com>
2021-11-29 13:40:34 -0600

    feat(hub2): add dictionary entry/entries

--- packages/hub2/src/index.ts
+++ packages/hub2/src/index.ts
@@ -1,5 +1,9 @@
 import telemetryDictionary from './dictionary.json';
 import telemetryConstants from './constants.json';
+import dimensions from './dimensions.json';
+import * as base from '@juliannemarik/telemetry-dictionary-base';
 
 export const dictionary = telemetryDictionary;
 export const constants = telemetryConstants;
+export const read = base.read.bind({ dimensions });
+export const write = base.write;
