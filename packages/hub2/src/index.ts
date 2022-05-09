f9ccd5ed27345fcf4b42a42e4cc97a6555074fad
juliannemarik <julianne.marik@gmail.com>
2022-05-09 12:16:56 -0500

    feat(hub2): add a new dictionary
    BREAKING CHANGE: removes something important

--- packages/hub2/src/index.ts
+++ packages/hub2/src/index.ts
@@ -3,6 +3,7 @@ import telemetryConstants from './constants.json';
 import dimensions from './dimensions.json';
 import * as base from '@juliannemarik/telemetry-dictionary-base';
 
+// testing
 export const dictionary = telemetryDictionary;
 export const constants = telemetryConstants;
 export const read = base.read.bind({ dimensions });
